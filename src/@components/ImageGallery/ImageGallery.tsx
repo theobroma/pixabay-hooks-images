f13ef4347cd5b10c38569fa770b750173df84b1c
Alexandr <theobroma222@gmail.com>
2021-04-06 17:49:52 +0300

    responsive ImageGallery

--- src/@components/ImageGallery/ImageGallery.tsx
+++ src/@components/ImageGallery/ImageGallery.tsx
@@ -1,3 +1,4 @@
+// Responsive approach https://www.tilcode.com/react-media-query-hook-responsive-material-ui-gridlist/
 // const ImageGallery = ({ picture, setImageData }) => {
 //   return (
 //     <ul className={s.ImageGallery}>
@@ -15,26 +16,108 @@
 // };
 // export default ImageGallery;
 import * as React from 'react';
+import {
+  GridList,
+  GridListTile,
+  GridListTileBar,
+  IconButton,
+  makeStyles,
+  Theme,
+  useMediaQuery,
+  useTheme,
+} from '@material-ui/core';
 import { useSelector } from 'react-redux';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 import { picturesSelector } from '../../@store/pictures/selectors';
-import ImageGalleryItem from './ImageGalleryItem';
+// import ImageGalleryItem from './ImageGalleryItem';
 
-export const ImageGallery: React.FC = () => {
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: 'auto',
+    height: 'auto',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  icon: {
+    color: 'white',
+  },
+}));
+
+export const ImageGallery: React.FC = (props: any) => {
   const picturesData = useSelector(picturesSelector).data;
   const picturesHits = picturesData.hits;
+
+  const classes = useStyles();
+  const theme = useTheme();
+
+  //   RESPONSIVE HACK
+  const screenExtraLarge = useMediaQuery(theme.breakpoints.only('xl'));
+  const screenLarge = useMediaQuery(theme.breakpoints.only('lg'));
+  const screenMedium = useMediaQuery(theme.breakpoints.only('md'));
+  const screenSmall = useMediaQuery(theme.breakpoints.only('sm'));
+  const screenExtraSmall = useMediaQuery(theme.breakpoints.only('xs'));
+  const screenNarrow = useMediaQuery('(max-width:340px)');
+
+  const getScreenWidth = () => {
+    if (screenExtraLarge) {
+      return 5;
+    }
+    if (screenLarge) {
+      return 4;
+    }
+    if (screenMedium) {
+      return 3;
+    }
+    if (screenSmall) {
+      return 2;
+    }
+    if (screenExtraSmall) {
+      return 1;
+    }
+    if (screenNarrow) {
+      return 1;
+    }
+    return 1;
+  };
+  //   END of RESPONSIVE HACK
+
   return (
-    <div>
-      <span>gallery</span>
-      {picturesHits?.map((item) => (
-        <ImageGalleryItem
-          //   onSetImageData={setImageData}
-          key={item.id}
-          picture={item}
-          //   src={webformatURL}
-          //   largeImageURL={largeImageURL}
-          //   tags={tags}
-        />
-      ))}
+    <div className={classes.root}>
+      <GridList
+        cellHeight={260}
+        cols={getScreenWidth()}
+        spacing={1}
+        className={classes.gridList}
+      >
+        {picturesHits?.map((tile) => (
+          <GridListTile key={tile.id} cols={1} rows={1}>
+            <img src={tile.webformatURL} alt="none" />
+            <GridListTileBar
+              title="title"
+              titlePosition="top"
+              actionIcon={
+                <IconButton aria-label="star title" className={classes.icon}>
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+              className={classes.titleBar}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
     </div>
   );
 };
