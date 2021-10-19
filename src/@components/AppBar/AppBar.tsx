6b05854a2094de3ec4366a3a9446b9ffa88ab37c
Alexandr Siryj <theobroma222@gmail.com>
2021-10-19 13:50:32 +0300

    rm unused appbar styles

--- src/@components/AppBar/AppBar.tsx
+++ src/@components/AppBar/AppBar.tsx
@@ -13,25 +13,10 @@ const useStyles = makeStyles((theme: Theme) => {
       grow: {
         flexGrow: 1,
       },
-      menuButton: {
-        marginRight: theme.spacing(2),
-      },
       title: {
         display: 'none',
         [theme.breakpoints.up('sm')]: {
           display: 'block',
-        },
-      },
-      sectionDesktop: {
-        display: 'none',
-        [theme.breakpoints.up('md')]: {
-          display: 'flex',
-        },
-      },
-      sectionMobile: {
-        display: 'flex',
-        [theme.breakpoints.up('md')]: {
-          display: 'none',
         },
       },
     }),
