19bced2384aa80836a232bbef72f416109bd5c66
Alexandr Siryj <theobroma222@gmail.com>
2022-07-21 05:05:48 +0300

    cleanup unused views

--- src/@routes/AppContainer.tsx
+++ src/@routes/AppContainer.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import AppModal from '../@features/AppModal';
-// import MainView from '../@views/MainView';
-// import MainView2 from '../@views/MainView2';
-import MainView3 from '../@views/MainView3';
+import MainView from '../@views/MainView';
+
 import { AppLayout } from './AppLayout';
 
 export const AppContainer = () => {
   return (
     <AppLayout>
-      {/* <MainView /> */}
-      {/* <MainView2 /> */}
-      <MainView3 />
+      <MainView />
       <AppModal />
     </AppLayout>
   );
--- src/@views/MainView/MainView.tsx
+++ src/@views/MainView/MainView.tsx
@@ -1,85 +1,62 @@
-// https://stackoverflow.com/questions/69499487/rtk-query-pagination-and-combine-queries
-// import { Box, Container } from '@material-ui/core';
+import { Container, Box } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-// import ImageGallery from '../../@components/ImageGallery';
-// import LoadingPage from '../../@components/UI/LoadingPage';
-// import { useAppDispatch } from '../../@store/configureStore';
-// // import { useGetPicturesQuery } from '../../@store/pictures/api';
-// import { HitsEntityType } from '../../@types';
-// import LoadMoreButton from '../../@components/LoadMoreButton';
-
-// const MainView = () => {
-//   const dispatch = useAppDispatch();
-
-//   const [noMoreResults, setNoMoreResults] = useState(false);
-//   const [page, setPage] = useState(1);
-//   const [hits, setHits] = useState<HitsEntityType[]>([]);
-
-//   const { data, error, isLoading, isFetching, isError } = useGetPicturesQuery(
-//     page,
-//     {
-//       skip: noMoreResults,
-//     },
-//   );
-
-//   // const {
-//   //   loading: picturesLoading,
-//   //   page,
-//   //   pictureSearch,
-//   //   data: { hits },
-//   // } = useAppSelector(picturesSelector);
-
-//   // const { data, error, isLoading, isFetching } = useGetPicturesQuery(page);
-
-//   // const data = [] as any;
-//   // const isFetching = true;
-
-//   // const hits = data?.hits;
-
-//   // useEffect(() => {
-//   //   dispatch(picturesTC({ pictureSearch, page }));
-//   // }, [dispatch, pictureSearch, page]);
-
-//   useEffect(() => {
-//     if (data?.hits?.length) {
-//       setHits([...hits, ...data.hits]);
-//     } else if (page > 1) {
-//       setNoMoreResults(true);
-//     }
-//   }, [data]); // yes!'data' only
-
-//   // useEffect(() => {
-//   //   console.log(data);
-//   //   console.log(hits);
-//   // }, [hits, data]);
-
-//   useEffect(() => {
-//     window.scrollTo({
-//       top: document.documentElement.scrollHeight,
-//       behavior: 'smooth',
-//     });
-//   }, [hits]);
-
-//   const handleLoadMore = () => {
-//     // dispatch(incrementPage());
-//     setPage(page + 1);
-//   };
-
-//   if (isError) return <div>An error has occurred!</div>;
-
-//   return (
-//     <Container maxWidth={false}>
-//       {isFetching && <LoadingPage />}
-//       {hits && <ImageGallery hits={hits} />}
-//       <Box my={3}>
-//         <LoadMoreButton isFetching={isFetching} handleClick={handleLoadMore} />
-//       </Box>
-//     </Container>
-//   );
-// };
+import ImageGallery from '../../@components/ImageGallery';
+import LoadMoreButton from '../../@components/LoadMoreButton';
+import LoadingPage from '../../@components/UI/LoadingPage';
+import { useAppDispatch, useAppSelector } from '../../@store/configureStore';
+import { picturesSelector } from '../../@store/pictures/selectors';
+import { picturesTC } from '../../@store/pictures/slice';
 
 const MainView = () => {
-  return <div>MainView</div>;
+  const dispatch = useAppDispatch();
+
+  const {
+    data: { total, totalHits, hits },
+    error,
+    isError,
+    isFetching,
+    pictureSearch,
+  } = useAppSelector(picturesSelector);
+
+  const [page, setPage] = useState(1);
+
+  // fetch after search changed
+  useEffect(() => {
+    setPage(1); // reset page
+    dispatch(picturesTC({ page: 1 })); // works also as initial fetch
+  }, [dispatch, pictureSearch]); // without page
+
+  const handleLoadMore = () => {
+    // dispatch(incrementPage());
+    setPage(page + 1);
+    dispatch(picturesTC({ page: page + 1 }));
+  };
+
+  useEffect(() => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: 'smooth',
+    });
+  }, [hits]);
+
+  if (isError)
+    return (
+      <div>
+        An error has occurred!
+        <br />
+        {error}
+      </div>
+    );
+
+  return (
+    <Container maxWidth={false}>
+      {isFetching && <LoadingPage />}
+      {hits && <ImageGallery hits={hits} />}
+      <Box my={3}>
+        <LoadMoreButton isFetching={isFetching} handleClick={handleLoadMore} />
+      </Box>
+    </Container>
+  );
 };
 
 export default MainView;
